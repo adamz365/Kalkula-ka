269d6cd2e427b0707d501a4f226e4291fb4c6562
adamz365 <131455481+adamz365@users.noreply.github.com>
2024-02-12 07:35:26 +0100

    Delete Kalkulačka/Kalkulačka.php

--- Kalkulačka/Kalkulačka.php
+++
@@ -1,9 +0,0 @@
-<form action="foo.php" method="post">
-    Name:  <input type="text" name="username" /><br />
-    Email: <input type="text" name="email" /><br />
-    <input type="submit" name="submit" value="Submit me!" />
-</form>
-
-<?php
-
-?>
